f0a915f43a00cdf257258e610a0ec40b6dead4fe
jatinparmar24 <jatinparmar2424@gmail.com>
2025-08-13 13:51:57 +0530

    underwater city landing page

--- flip.js
+++ flip.js
@@ -11,3 +11,21 @@ function closePopup(side) {
 }
 
 // section 1
+
+
+// section 2
+const bubblesContainer = document.querySelector('.under-bubbles');
+
+for (let i = 0; i < 20; i++) {
+  const bubble = document.createElement('div');
+  bubble.classList.add('bubble');
+  const size = Math.random() * 20 + 10 + 'px';
+  bubble.style.width = size;
+  bubble.style.height = size;
+  bubble.style.left = Math.random() * 100 + '%';
+  bubble.style.animationDuration = Math.random() * 5 + 5 + 's';
+  bubble.style.animationDelay = Math.random() * 5 + 's';
+  bubblesContainer.appendChild(bubble);
+}
+
+// section 2
