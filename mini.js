dda47d7f9d4c87df9e3f0f09d0fea8a59409ca72
jatinparmar24 <jatinparmar2424@gmail.com>
2025-09-16 15:48:11 +0530

    new spa

---
+++ mini.js
@@ -0,0 +1,10 @@
+// section 1
+const hamburger = document.getElementById("hamburger");
+const links = document.querySelector(".daily-links");
+
+hamburger.addEventListener("click", () => {
+  links.classList.toggle("show");
+});
+
+
+// section 1
